a782b506f5d9a8f5b6d3dea4a1a3e59ac6757f09
Tanaka Kakono <pittkayz@gmail.com>
2024-08-25 02:26:54 +0200

    Appended

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,8 +5,44 @@ const additional = document.getElementById('add')
 function Select(){
     if(select.value === "business" || select.value === "pro" || select.value === "medics"){
         additional.disabled = false;
-        display.innerHTML = select.value;
     }else{
         additional.disabled = true;
     }
 }
+
+$(document).ready(() => {
+    $("#webdev").show();
+        $("#e-commerce").hide();
+        $("#ads").hide();
+        $("#seo").hide();
+})
+
+$(document).ready(function(){
+    $("#webdev-button").click(function(){
+        $("#webdev").show();
+        $("#e-commerce").hide();
+        $("#ads").hide();
+        $("#seo").hide();
+    });
+
+    $("#ecom-button").click(function(){
+        $("#webdev").hide();
+        $("#e-commerce").show();
+        $("#ads").hide();
+        $("#seo").hide();
+    });
+
+    $("#ads-button").click(function(){
+        $("#webdev").hide();
+        $("#e-commerce").hide();
+        $("#ads").show();
+        $("#seo").hide();
+    });
+
+    $("#seo-button").click(function(){
+        $("#webdev").hide();
+        $("#e-commerce").hide();
+        $("#ads").hide();
+        $("#seo").show();
+    });
+})
